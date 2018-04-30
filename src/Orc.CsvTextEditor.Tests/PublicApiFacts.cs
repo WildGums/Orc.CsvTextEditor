990b6d429629a4c0f66f70d3c6f166cc6649bdfe
Geert van Horrik <geert@catenalogic.com>
2018-04-30 19:24:07 +0200

    Migrate test project to new project structure

--- src/Orc.CsvTextEditor.Tests/PublicApiFacts.cs
+++ src/Orc.CsvTextEditor.Tests/PublicApiFacts.cs
@@ -7,13 +7,14 @@
 
 namespace Orc.CsvTextEditor.Tests
 {
+    using System.Runtime.CompilerServices;
     using ApiApprover;
     using NUnit.Framework;
 
     [TestFixture]
     public class PublicApiFacts
     {
-        [Test]
+        [Test, MethodImpl(MethodImplOptions.NoInlining)]
         public void Orc_CsvTextEditor_HasNoBreakingChanges()
         {
             var assembly = typeof(CsvTextEditorControl).Assembly;
