6bb453cecf2a43dfb9946e1f788a2f1c503412b6
Geert van Horrik <geert@catenalogic.com>
2024-07-07 22:31:39 +0200

    Fix tests

--- src/SolutionAssemblyInfo.cs
+++ src/SolutionAssemblyInfo.cs
@@ -6,8 +6,8 @@
 using System.Reflection;
 
 [assembly: AssemblyCompany("WildGums")]
-[assembly: AssemblyVersion("5.0.0")]
-[assembly: AssemblyFileVersion("5.0.0")]
-[assembly: AssemblyInformationalVersion("5.0.0-alpha.78")]
+[assembly: AssemblyVersion("5.1.0")]
+[assembly: AssemblyFileVersion("5.1.0")]
+[assembly: AssemblyInformationalVersion("5.1.0-alpha.78")]
 [assembly: AssemblyCopyright("Copyright © WildGums 2014 - 2019")]
 
